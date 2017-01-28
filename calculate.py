becb6728500dcde9e5ea2b366061fcd3418d8c42
Wim Ulkeman <wimulkeman@hotmail.com>
2017-01-29 00:19:47 +0100

    Adjustment for shifting the bits. The pieces are now placed into the board in the test run

--- calculate.py
+++ calculate.py
@@ -13,16 +13,22 @@ solution.set_board(play_board['board'])
 
 found_solutions = 0
 
+# Use the total board length to calculate the first available spot in reverse
+total_board_length = len(bin(play_board['board'])[2:])
+
 for label, piece in pieces.get_pieces().iteritems():
-    available_spot = bin(solution.get_board())[2:].find('0')
+    # Get the last 0 in the bit string which indicates a free spot
+    available_spot = bin(solution.get_board())[2:].rfind('0')
     if not available_spot:
         break
+    available_spot = total_board_length - available_spot - 1
 
+    # Check if one of the positions of the piece fits into the free spots.
     for piece_position in piece:
-        piece_position << available_spot
-        if solution.does_piece_fit(piece_position):
+        new_position = piece_position << available_spot
+        if solution.does_piece_fit(new_position):
             print 'fit'
-            solution.add_piece(piece_position)
+            solution.add_piece(new_position)
             break
         else:
             continue
