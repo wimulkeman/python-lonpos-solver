72aed2092fe08123d4bcd168108e4699a456c8ba
Wim Ulkeman <wimulkeman@hotmail.com>
2017-01-29 19:03:52 +0100

    Adding a function to find the solutions

--- calculate.py
+++ calculate.py
@@ -5,7 +5,8 @@ board = Board.Board()
 play_board = board.board_9x9()
 
 # Generate the pieces for the board
-pieces = Piece.Piece(play_board['line_length'])
+available_pieces = Piece.Piece(play_board['line_length'])
+print available_pieces.get_pieces()
 
 # Keep track of the generated solutions
 solution = Solution.Solution()
@@ -16,24 +17,33 @@ found_solutions = 0
 # Use the total board length to calculate the first available spot in reverse
 total_board_length = len(bin(play_board['board'])[2:])
 
-for label, piece in pieces.get_pieces().iteritems():
+def calculate_solution(board, pieces):
+    # When the function is called without any
+    if not pieces:
+        global found_solutions
+        found_solutions = found_solutions + 1
+        print "Solution found: " + str(found_solutions)
+        return
+
     # Get the last 0 in the bit string which indicates a free spot
-    available_spot = bin(solution.get_board())[2:].rfind('0')
+    available_spot = bin(board)[2:].rfind('0')
     if not available_spot:
-        break
+        return
+
     available_spot = total_board_length - available_spot - 1
 
-    # Check if one of the positions of the piece fits into the free spots.
-    for piece_position in piece:
-        new_position = piece_position << available_spot
-        if solution.does_piece_fit(new_position):
-            print 'fit'
-            solution.add_piece(new_position)
-            break
-        else:
-            continue
+    for label, piece in pieces.iteritems():
+        # Check if one of the positions of the piece fits into the free spots.
+        for piece_position in piece:
+            new_position = piece_position << available_spot
+            if (board & new_position) == 0:
+                # Create a new board by using the old board with the piece added to it
+                new_board = board + new_position
+                # Look for the rest of the board with all the pieces which haven't been evaluated
+                # yet
+                new_pieces = pieces.copy()
+                del new_pieces[label]
 
-print board.board_9x9()['board']
-print bin(board.board_9x9()['board'])
-print solution.get_board()
-print bin(solution.get_board())
+                calculate_solution(new_board, new_pieces)
+
+calculate_solution(play_board['board'], available_pieces.get_pieces())
