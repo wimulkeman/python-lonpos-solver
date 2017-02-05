91c7ade0ffd763f856124fc95ed88a7c3db22f29
Wim Ulkeman <wimulkeman@hotmail.com>
2017-02-06 00:09:00 +0100

    Adding multiprocessing. On the start, each piece entry gets is handled by a own process.
    Calculating with solution visualisation enabled now takes 11 minutes (previous 30 minutes)
    Calculating without solution visualisation now takes 4 minutes (previous 16 minutes)
    
    The number of used workers is equal to the number of pieces. Tests thus far showed the best performances with this setup. The performance depends on the used test rig.

--- calculate.py
+++ calculate.py
@@ -1,12 +1,13 @@
 from Lonpos import Board, Piece, Solution
 from timeit import default_timer as timer
 import datetime
-from multiprocessing import Pool
+import multiprocessing
 from functools import partial
 
 # Get the board on witch there will be played
 board = Board.Board()
-play_board = board.board_5x11()
+# play_board = board.board_5x11()
+play_board = board.board_9x9()
 
 # Generate the pieces for the board
 available_pieces = Piece.Piece(play_board['line_length'])
@@ -17,7 +18,7 @@ found_solutions = 0
 total_board_length = len(bin(play_board['board'])[2:])
 
 
-def calculate_solution(filled_board, pieces, new_board_solution, solution_instance):
+def calculate_solution(filled_board, pieces, new_board_solution, solution_instance, piece_character=None):
     # Get the last 0 in the bit string which indicates a free spot
     available_spot = bin(filled_board['board'])[2:].rfind('0')
     if not available_spot:
@@ -25,16 +26,19 @@ def calculate_solution(filled_board, pieces, new_board_solution, solution_instan
 
     # When the function is called without any
     if not pieces:
-        global found_solutions
-        found_solutions += 1
-        print ' ==== Solution: ' + str(found_solutions) + ' ==== '
+        print ' ======= Solution ======= '
         print solution_instance.draw_board(new_board_solution, filled_board['line_length'])
-        print ' ====================== '
+        print ' ======================== '
         return
 
     available_spot = total_board_length - available_spot - 1
 
     for label, piece in pieces.iteritems():
+        # Using this check it is possible to only process one character for the starting position
+        # of the board
+        if piece_character is not None and piece_character is not label:
+            continue
+
         # Check if one of the positions of the piece fits into the free spots.
         for piece_position in piece:
             new_position = piece_position << available_spot
@@ -61,10 +65,21 @@ if __name__ == '__main__':
     print '== The starting board =='
     print solution.draw_board(board_solution, play_board['line_length'])
 
-    start_time = timer()
-    calculate_solution(play_board, available_pieces.get_pieces(), board_solution, solution)
-    end_time = timer()
+    available_pieces_list = available_pieces.get_pieces()
 
-    print found_solutions
+    # Make a worker process for every core available
+    # pool = multiprocessing.Pool(multiprocessing.cpu_count() + 4)
+    pool = multiprocessing.Pool(len(available_pieces_list.keys()))
+
+    start_time = timer()
+
+    pool.map(
+        partial(calculate_solution, play_board, available_pieces_list, board_solution, solution),
+        available_pieces_list.keys()
+    )
+    pool.close()
+    pool.join()
+
+    end_time = timer()
 
     print str(datetime.timedelta(seconds=(end_time - start_time)))
